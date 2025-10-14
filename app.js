bfe831a1afc4b14f72e2b994679689415e5481c9
Paul Akintunde <paulakintunde06@gmail.com>
2025-10-14 11:06:43 +0100

    fixed production issue

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(helmet({
                 // ...(isProduction ? [] : ["'unsafe-inline'", "http://localhost:3000"])
                 "'unsafe-inline'",
                 // "http://localhost:3000", // Development
-                // "https://chatapp-mw90.onrender.com" // Production
+                "https://chatapp-mw90.onrender.com" // Production
             ],
             connectSrc: [
                 "'self'",
